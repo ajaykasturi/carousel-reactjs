5e5d962178b75c4a6c439c28abff2c05b926de92
Ajay <ajayreigns0@gmail.com>
2024-08-17 02:14:04 +0530

    improved carousel from  basic to advance

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,42 @@ import img1 from "./assets/img1.jpg";
 import img2 from "./assets/img2.jpg";
 import img3 from "./assets/img3.jpg";
 import img4 from "./assets/img4.jpg";
+import AdvancedCarousel from "./components/AdvancedCarousel";
 function App() {
   const slides = [img1, img2, img3, img4];
   return (
-    <div className="m-1 border-[10px] border-slate-950 flex flex-col justify-center items-center">
-      <div className="max-w-lg border-2 border-blue-500 w-full">
-        <Carousel autoSlide={true}>
-          {slides.map((src, index) => (
-            <img key={index} src={src} className="w-full" />
-          ))}
-        </Carousel>
+    <div className="m-1 border-[10px] border-slate-950 flex  justify-center items-center h-screen gap-x-4 bg-[#0c0c1d]">
+      <div className="flex flex-col gap-y-2">
+        <h1 className="text-center text-white font-semibold text-2xl">
+          Basic Carousel
+        </h1>
+        <div className="max-w-lg border-2 border-blue-500 w-full">
+          <Carousel autoSlide={true}>
+            {slides.map((src, index) => (
+              <img key={index} src={src} className="w-full" />
+            ))}
+          </Carousel>
+        </div>
+      </div>
+      <div className="flex flex-col gap-y-2">
+        <h1 className="text-center text-white font-semibold text-2xl">
+          Advanced Carousel
+        </h1>
+        <div className="max-w-lg border-2 border-blue-500 w-full">
+          <AdvancedCarousel autoSlide={true} autoSlideInterval={2000}>
+            {[
+              <img
+                key={"unqiue1"}
+                src={slides[slides.length - 1]}
+                className="w-full"
+              />,
+              ...slides.map((src, index) => (
+                <img key={index} src={src} className="w-full" />
+              )),
+              <img key={"unqiue2"} src={slides[0]} className="w-full" />,
+            ]}
+          </AdvancedCarousel>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/AdvancedCarousel.jsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from "react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+function AdvancedCarousel({
+  children: slides,
+  autoSlide = false,
+  autoSlideInterval = 3000,
+}) {
+  const [currIndex, setCurrIndex] = useState(1);
+  const [isTransitioning, setIsTransitioning] = useState(false);
+  const prev = () => {
+    if (isTransitioning) return;
+    setIsTransitioning(true);
+    setCurrIndex((curr) => (curr === 0 ? slides.length - 2 : curr - 1));
+  };
+  const next = () => {
+    if (isTransitioning) return;
+    setIsTransitioning(true);
+    setCurrIndex((curr) => (curr === slides.length - 1 ? 1 : curr + 1));
+  };
+  const handleTransitionEnd = () => {
+    setIsTransitioning(false);
+    if (currIndex === slides.length - 1) {
+      setCurrIndex(1);
+    } else if (currIndex === 0) {
+      setCurrIndex(slides.length - 2);
+    }
+  };
+  useEffect(() => {
+    if (!autoSlide) return;
+    const slideInterval = setInterval(() => {
+      next();
+    }, autoSlideInterval);
+    return () => clearInterval(slideInterval);
+  }, [next]);
+  return (
+    <div className="overflow-hidden relative">
+      <div
+        onTransitionEnd={handleTransitionEnd}
+        className={`flex ${
+          isTransitioning ? "transition-transform duration-500 ease-out" : ""
+        }`}
+        style={{ transform: `translateX(-${currIndex * 100}%)` }}
+      >
+        {slides}
+      </div>
+      <div className="absolute inset-0 flex items-center justify-between p-2">
+        <button
+          onClick={prev}
+          className="p-1 rounded-full shadow-sm bg-white/80 hover:bg-white text-gray-800"
+        >
+          <ChevronLeft size={30} />
+        </button>
+        <button
+          onClick={next}
+          className="p-1 rounded-full shadow-sm bg-white/80 hover:bg-white text-gray-800"
+        >
+          <ChevronRight size={30} />
+        </button>
+      </div>
+      <div className="absolute bottom-4 right-0 left-0">
+        <div className="flex justify-center items-center gap-x-2">
+          {slides.map((_, i) => {
+            if (i == 0 || i == slides.length - 1) return;
+            return (
+              <div
+                key={`${_}${i}`}
+                onClick={() => setCurrIndex(i)}
+                className={`cursor-pointer ${
+                  true ? "transition-all duration-500 ease-out" : ""
+                } w-2 h-2  bg-white rounded-full ${
+                  currIndex == i ? "p-[0.4rem]" : "bg-opacity-50"
+                }`}
+              ></div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AdvancedCarousel;
